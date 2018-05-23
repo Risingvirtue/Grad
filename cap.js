6c4f7f4d8c06b81e46b8cb8026197ba5cedc0288
Risingvirtue <risingvirtue@berkeley.edu>
2018-05-22 20:25:59 -0700

    trash

--- cap.js
+++ cap.js
@@ -1,5 +1,7 @@
 var angle = 0;
-
+const grav = -9.8;
+var velocity = 10;
+var t = 0;
 $(document).ready(function() {
 	console.log('hi');
 })
@@ -11,12 +13,19 @@ function throwCap() {
 	setInterval(spin, 0);
 }
 
+function moveUp() {
+	
+	
+	t = t + 0.01;
+	
+}
 function spin() {
+	moveUp();
 	angle += 1;
 	$("#cap").css({'transform': 'rotate(' + angle + 'deg)'});
-	var lesserAngle = angle % 90;
+	var lesserAngle = angle % 45;
 	console.log(lesserAngle);
-	$(".circle-container").css({'transform': 'rotate(-'  + (angle  - lesserAngle) + 'deg)'})
+	$(".circle-container").css({'transform': 'rotate(-'  + (angle) + 'deg)'})
 	
 }
 
